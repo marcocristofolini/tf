0d2d1bb69fbef37d573254cf9e4d897c658c07ff
Marco Cristofolini <mf.cristofolini@gmail.com>
2022-03-17 17:31:32 -0300

    Delete app_service directory

--- app_service/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "azurerm_app_service_tfer--front-wfp_id" {
-  value = "${azurerm_app_service.tfer--front-wfp.id}"
-}
-
-output "azurerm_app_service_tfer--ms-wfp-backend_id" {
-  value = "${azurerm_app_service.tfer--ms-wfp-backend.id}"
-}
